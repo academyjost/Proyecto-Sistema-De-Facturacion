7c888a7b2d727da4fae9d9336f82f258f50d6ec9
jazzynpro <reader_jaz@hotmail.com>
2026-06-25 21:34:41 -0500

    Agregado id en producto y tabla, validacion de no repeticion de id

--- datosPrueba.js
+++ datosPrueba.js
@@ -28,46 +28,55 @@ const clientesPrueba = [
 
 const productosPrueba = [
     {
+        id: 10001,
         nombre: "CREMA CERAMID",
         precio: 18,
         stock: 4
     },
     {
+        id: 10002,
         nombre: "CREMA NIVEA",
         precio: 8,
         stock: 3
     },
     {
+        id: 10003,
         nombre: "PEDIALITE",
         precio: 4.75,
         stock: 10
     },
     {
+        id: 10004,
         nombre: "ENDEROGERMINA",
         precio: 3,
         stock: 25
     },
     {
+        id: 10005,
         nombre: "PASTA DE DIENTES",
         precio: 1,
         stock: 18
     },
     {
+        id: 10006,
         nombre: "PARACETAMOL",
         precio: 0.10,
         stock: 30
     },
     {
+        id: 10007,
         nombre: "CEPILLO DE DIENTES",
         precio: 1,
         stock: 10
     },
     {
+        id: 10008,
         nombre: "JABON NIVEA",
         precio: 2.8,
         stock: 5
     },
     {
+        id: 10009,
         nombre: "ASPIRINA",
         precio: 0.50,
         stock: 20
